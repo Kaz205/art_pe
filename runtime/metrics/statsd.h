e239fe7ddffe9892281601526f64bec1692f609b
Kazuki Hashimoto <kazukih@tuta.io>
2023-03-01 00:51:16 +0900

    Revert "Write boot image status to statsd."
    This reverts commit 8b89c39ac0d7a49216fa7eaabf9d582f1143470d.

--- runtime/metrics/statsd.h
+++ runtime/metrics/statsd.h
@@ -27,10 +27,8 @@ class MetricsBackend;
 // Statsd is only supported on Android
 #ifdef __ANDROID__
 std::unique_ptr<MetricsBackend> CreateStatsdBackend();
-void ReportDeviceMetrics();
 #else
 inline std::unique_ptr<MetricsBackend> CreateStatsdBackend() { return nullptr; }
-inline void ReportDeviceMetrics() {}
 #endif
 
 }  // namespace metrics
